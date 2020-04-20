5000f890f22c6a6c5c1f7c902bae7cb73b55037d
Ruslan Tisanyk <ruslantisanykot@gmail.com>
2020-04-20 19:00:33 +0300

    added script for task-4

---
+++ module-01/home-work/excercise-04/task-4.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let credits = 23580;
+
+const pricePerDroid = 3000;
+
+const quantityGoods = prompt(
+  'Укажите колличество дроидов, которое желаете приобрести',
+);
+
+let totalPrice;
+
+if (quantityGoods === null) {
+  alert('Отменено пользователем!');
+} else {
+  totalPrice = quantityGoods * pricePerDroid;
+}
+
+if (credits >= totalPrice) {
+  credits -= totalPrice;
+  alert(
+    `Вы купили ${quantityGoods} дроидов, на счету осталось ${credits} кредитов.`,
+  );
+} else {
+  alert('Недостаточно средств на счету!');
+}
