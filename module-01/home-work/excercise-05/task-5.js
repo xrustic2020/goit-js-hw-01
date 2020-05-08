9095187f091a0e461ed955493c61abbfb8fcd754
Ruslan Tisanyk <ruslantisanykot@gmail.com>
2020-05-08 17:42:03 +0300

    transfer allert outline switch

--- module-01/home-work/excercise-05/task-5.js
+++ module-01/home-work/excercise-05/task-5.js
@@ -7,39 +7,26 @@ let deliveryPrice;
 switch (deliveryCountry.toLowerCase()) {
   case 'китай':
     deliveryPrice = 100;
-    alert(
-      `Доставка в ${deliveryCountry} будет стоить ${deliveryPrice} кредитов`,
-    );
     break;
 
   case 'чили':
     deliveryPrice = 250;
-    alert(
-      `Доставка в ${deliveryCountry} будет стоить ${deliveryPrice} кредитов`,
-    );
     break;
 
   case 'австралия':
     deliveryPrice = 170;
-    alert(
-      `Доставка в ${deliveryCountry} будет стоить ${deliveryPrice} кредитов`,
-    );
     break;
 
   case 'индия':
     deliveryPrice = 80;
-    alert(
-      `Доставка в ${deliveryCountry} будет стоить ${deliveryPrice} кредитов`,
-    );
     break;
 
   case 'ямайка':
     deliveryPrice = 120;
-    alert(
-      `Доставка в ${deliveryCountry} будет стоить ${deliveryPrice} кредитов`,
-    );
     break;
 
   default:
     alert('В вашей стране доставка не доступна');
 }
+
+alert(`Доставка в ${deliveryCountry} будет стоить ${deliveryPrice} кредитов`);
